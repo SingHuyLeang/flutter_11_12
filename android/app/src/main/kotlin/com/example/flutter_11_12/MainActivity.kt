1a985f8a8a5507a60dc4affd13643073b85e8600
SingHuyLeang <leang6525@gmail.com>
2024-05-23 12:09:08 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_11_12/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_11_12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/category.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  IconData icon;
+  String title;
+  Category({required this.icon,required this.title});
+}
